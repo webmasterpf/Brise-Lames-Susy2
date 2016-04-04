8291f29e8b206ff74bc8fb5ebb10d9dcf67493d5
webmasterpf-DG <webmaster@provence-formation.com>
2016-04-04 16:05:45 +0200

    PROD: correction diverses

--- page-page_projets.tpl.php
+++ page-page_projets.tpl.php
@@ -52,12 +52,6 @@ include ($theme_path.'/includes/inc_header.php');
           
       </div> <!-- /content-inner /content -->
 
-        <?php if (!empty($primary_links) or !empty($secondary_links)): ?>
-          <div id="navigation" class="menu <?php if (!empty($primary_links)) { print "with-main-menu"; } if (!empty($secondary_links)) { print " with-sub-menu"; } ?>">
-            <?php if (!empty($primary_links)){ print theme('links', $primary_links, array('id' => 'primary', 'class' => 'links main-menu')); } ?>
-            <?php if (!empty($secondary_links)){ print theme('links', $secondary_links, array('id' => 'secondary', 'class' => 'links sub-menu')); } ?>
-          </div> <!-- /navigation -->
-        <?php endif; ?>
 
         <!-- ______________________ CONTENU BAS _______________________ -->
 <?php if ($content_bottom): ?>
